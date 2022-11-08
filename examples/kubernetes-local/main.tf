f37fe945aa099e1edc916e96469ff3b0ce5a28ae
Erni <ernesto.reig@gmail.com>
2022-11-08 09:05:16 +0100

    fixed typo

--- examples/kubernetes-local/main.tf
+++ examples/kubernetes-local/main.tf
@@ -8,7 +8,7 @@ provider "kubernetes" {
 module "simple_webapp" {
   source = "../../modules/services/k8s-app"
 
-  name           = "simple-ewbapp"
+  name           = "simple-webapp"
   image          = "training/webapp"
   replicas       = 2
   container_port = 5000
